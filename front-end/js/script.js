c170c8fa7696ae3798da6b6ab0ff940ab6d30283
Brock Gledhill <brock.gledhill@office.ereplacement1.com>
2017-04-20 16:09:31 -0600

    css changes

--- front-end/js/script.js
+++ front-end/js/script.js
@@ -1,25 +1,34 @@
+// Mobile Navbar slide down \\
+$('.mobile-navbar').click(function() {
+    $('.navbar').slideToggle(500);
+});
+
 // Scroll to Element with Logo and Title Animations \\
 $('a[href*="#"]:not([href="#"])').click(function() {
 
     // Logo and Title \\
-    $("#main-logo").fadeOut(1000);
-    $(".name-container").animate({"margin-left": "-25px"}, 2000);
-    $(".title").animate({"margin-left": "-80px"}, 1000);
-    $("header").addClass("header-overlay", function() {
-      $(this).fadeIn(2000);
-    });
-    $("#header-logo").delay(500).fadeIn(2000);
-    
+    // $("#main-logo").fadeOut(500);
+    // $(".name-container").animate({"margin-left": "-25px"}, 1000);
+    // $(".title").animate({"margin-left": "-80px"}, 500);
+    // $("header").addClass("header-overlay", function() {
+    //   $(this).fadeIn(1000);
+    // });
+    // $("#header-logo").delay(250).fadeIn(500);
+    $('.navbar').slideToggle(500);
+
     // Scroll \\
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
         var target = $(this.hash);
         target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
         if (target.length) {
-            $('html,body').delay(1500).animate({scrollTop: target.offset().top}, 1500);
+            $('html,body').delay(250).animate({
+                scrollTop: target.offset().top-85
+            }, 1500);
             return false;
         }
     }
 });
+
 
 // Active Nav Tabs \\
 $('.toggle-tabs').click(function(e) {
